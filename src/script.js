508602839a705bb27ecdfadfd46552e88fcd9241
JulietBrn <shishkina.ja@gmail.com>
2025-03-27 15:59:17 +0100

    feat: add animation

--- src/script.js
+++ src/script.js
@@ -1,30 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   /* animation */
-  // const animItems = document.querySelectorAll(".animate");
-  // const classAnimation = "fade-to-right"; //set class animation
-  // if (animItems.length) {
-  //   function handleIntersection(entries, observer) {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         entry.target.classList.add(classAnimation);
-  //         observer.unobserve(entry.target);
-  //       }
-  //     });
-  //   }
-  //   const observer = new IntersectionObserver(handleIntersection, {
-  //     threshold: 0.1,
-  //   });
-  //   animItems.forEach((animation) => {
-  //     observer.observe(animation);
-  //   });
-  // }
-  // /* FAQ */
-  // const faqItems = document.querySelectorAll(".faq-item");
-  // if (faqItems.length) {
-  //   faqItems.forEach((item) => {
-  //     item.addEventListener("click", () => {
-  //       item.classList.toggle("active");
-  //     });
-  //   });
-  // }
+  const animItems = document.querySelectorAll("h2, h1, .product, .review ");
+  const classAnimation = "magic"; //set class animation
+  if (animItems.length) {
+    function handleIntersection(entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(classAnimation);
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.1,
+    });
+    animItems.forEach((animation) => {
+      observer.observe(animation);
+    });
+  }
 });
